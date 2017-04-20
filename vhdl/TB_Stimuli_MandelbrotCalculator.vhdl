029d90086ad7a1f1c87c5b865f70d99d7716d7ad
Jonathan Dreyer <jonathan.dreyer@cleanenergie.ch>
2017-04-20 15:47:11 +0200

    Edit add test case in testbench

--- vhdl/TB_Stimuli_MandelbrotCalculator.vhdl
+++ vhdl/TB_Stimuli_MandelbrotCalculator.vhdl
@@ -35,6 +35,10 @@ architecture Behavioral of TB_Stimuli_IP_MandelbrotCalculator is
   	SIGNAL error_number : INTEGER   := 0;
   	SIGNAL clk_gen      : std_logic := '0';
 
+    signal iter_ref     : std_logic_vector(SIZE_ITER-1 downto 0);
+    signal z_re_1_ref   : std_logic_vector(SIZE-1 downto 0);
+    signal z_im_1_ref   : std_logic_vector(SIZE-1 downto 0);
+
 begin
 
   --Intanciation du composant UUT
@@ -66,9 +70,12 @@ begin
       PROCEDURE init IS
         BEGIN
           rst_o     <= '1';
-          start_o  <= '0';
+          start_o   <= '0';
           c_re_o    <= (OTHERS => '0');
           c_im_o    <= (OTHERS => '0');
+          iter_ref  <= (OTHERS => '0');
+          z_re_1_ref <= (OTHERS => '0');
+          z_im_1_ref <= (OTHERS => '0');
       END init;
 
       --********** PROCEDURE "assign_mandelbrot" **********
@@ -77,6 +84,14 @@ begin
           c_re_o    <= c_re;
           c_im_o    <= c_im;
       END assign_mandelbrot;
+
+      --********** PROCEDURE "assign_ref_mandelbrot" **********
+      PROCEDURE assign_ref_mandelbrot(iteration_ref: IN std_logic_vector(SIZE_ITER-1 DOWNTO 0); z_re_ref, z_im_ref: IN std_logic_vector(SIZE-1 DOWNTO 0)) IS
+        BEGIN
+          iter_ref <= iteration_ref;
+          z_re_1_ref <= z_re_ref;
+          z_im_1_ref <= z_im_ref;
+      END assign_ref_mandelbrot;
 
       --********** PROCEDURE "execute_mandebrot" **********
       PROCEDURE execute_mandebrot IS
@@ -88,19 +103,39 @@ begin
           sim_cycle(150);
       END execute_mandebrot;
 
-      --********** PROCEDURE "check_result_mandelbrot" **********
-      PROCEDURE check_result_mandelbrot(nb_iteration_attendu: IN std_logic_vector(SIZE_ITER-1 DOWNTO 0); z_re_attendu, z_im_attendu: IN std_logic_vector(SIZE-1 DOWNTO 0); erreur: IN integer) IS
+      --********** PROCEDURE "check_result_mandelbrot_light" **********
+      PROCEDURE check_result_mandelbrot_light(erreur: IN integer) IS
         BEGIN
-          IF nb_iteration_attendu/=iteration_i THEN
+          IF iter_ref/=iteration_i THEN
             mark_error <= '1', '0' AFTER 1 ns;
             error_number <= erreur;
             REPORT "Erreur No " & integer'image(erreur) & " - Etat du resultat (iteration) non correct" SEVERITY ERROR;
           END IF;
 
-          --TODO z_re & z_im
+          sim_cycle(1);
+      END check_result_mandelbrot_light;
+
+      --********** PROCEDURE "check_result_mandelbrot_full" **********
+      PROCEDURE check_result_mandelbrot_full(erreur: IN integer) IS
+        BEGIN
+          IF iter_ref/=iteration_i THEN
+            mark_error <= '1', '0' AFTER 1 ns;
+            error_number <= erreur;
+            REPORT "Erreur No " & integer'image(erreur) & " - Etat du resultat (iteration) non correct" SEVERITY ERROR;
+          END IF;
+          IF z_re_1_ref/=z_re_i THEN
+            mark_error <= '1', '0' AFTER 1 ns;
+            error_number <= erreur;
+            REPORT "Erreur No " & integer'image(erreur) & " - Etat du resultat (re) non correct" SEVERITY ERROR;
+          END IF;
+          IF z_im_1_ref/=z_im_i THEN
+            mark_error <= '1', '0' AFTER 1 ns;
+            error_number <= erreur;
+            REPORT "Erreur No " & integer'image(erreur) & " - Etat du resultat (im) non correct" SEVERITY ERROR;
+          END IF;
 
           sim_cycle(1);
-      END check_result_mandelbrot;
+      END check_result_mandelbrot_full;
 
       BEGIN
 
@@ -113,13 +148,17 @@ begin
         rst_o <= '0';
         sim_cycle(1);
 
+        --C_Re = 0.0, C_Im = 0.0 -> Iteration = 100
         assign_mandelbrot("00" & X"0000", "00" & X"0000");
+        assign_ref_mandelbrot(X"64", "00" & X"2000", "00" & X"0000");
         execute_mandebrot;
-        check_result_mandelbrot(X"00", "00" & X"2000", "00" & X"0000", 100);
+        check_result_mandelbrot_light(1);
 
-        assign_mandelbrot("00" & X"0000", "00" & X"0000");
+        --C_Re = 2.0, C_Im = 2.0 -> Iteration = 1
+        assign_mandelbrot("00" & X"4000", "00" & X"4000");
+        assign_ref_mandelbrot(X"01", "00" & X"0000", "00" & X"2000");
         execute_mandebrot;
-        check_result_mandelbrot(X"00", "00" & X"0000", "00" & X"2000", 101);
+        check_result_mandelbrot_light(2);
 
 
         --End simu
